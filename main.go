10e13a103411af1aa93e453a9096f563493a928b
Fernando Sckaff <sckafffernando@gmail.com>
2024-10-20 22:02:30 -0700

    Making it SP

--- main.go
+++ main.go
@@ -3,18 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	_ "net/http/pprof"
 )
-import _ "net/http/pprof"
 
 func main() {
 	fmt.Println("hello")
-	hub := NewHub()
-	go hub.Run()
-
 	static := http.Dir("web/dist")
 
 	http.Handle("/", http.FileServer(static))
-	http.HandleFunc("/ws", hub.ServeWs)
-
 	http.ListenAndServe("localhost:8000", nil)
 }
