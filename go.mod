2db1139337803f1d382e4457f9d6330f268266c8
Fernando Sckaff <sckafffernando@gmail.com>
2024-10-22 17:36:39 -0700

    restarting

--- go.mod
+++
@@ -1,5 +0,0 @@
-module tank-ops
-
-go 1.22.5
-
-require github.com/gorilla/websocket v1.5.3
--- web/src/app.ts
+++ web/src/app.ts
@@ -1,7 +1,51 @@
-import { Game } from "./game.js";
+let message: string = 'test';
 
-const canvas = document.getElementById("game") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
 
-const game = new Game(ctx);
-game.run();
+// add the heading to the document
+document.body.appendChild(heading);
+
+// function to set background color based on window size
+function setBackgroundColor() {
+    if (window.innerWidth === 800 && window.innerHeight === 600) {
+        document.body.style.backgroundColor = "#909090";
+    } else {
+        document.body.style.backgroundColor = ""; // reset to default
+    }
+}
+
+// set initial background color
+setBackgroundColor();
+
+// add event listener for window resize
+window.addEventListener('resize', setBackgroundColor);
+
+function setBodySizeToWindow() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    document.body.style.width = `${width}px`;
+    document.body.style.height = `${height}px`;
+
+    console.log("Window inner width")
+    console.log(window.innerWidth)
+
+    console.log("Window outer height")
+    console.log(window.outerWidth)
+
+    console.log("Window inner height")
+    console.log(window.innerHeight)
+
+    console.log("Window outer height")
+    console.log(window.outerHeight)
+}
+
+// Call the function initially to set the size
+setBodySizeToWindow();
+
+// Update the size on window resize
+window.addEventListener('resize', setBodySizeToWindow);
+window.addEventListener('resize', setBackgroundColor);
+
